adcebf4daf058686ba3e5774e049ebb66b597943
Yüksel Güzel <yuksel.guzel@outlook.com>
2022-07-13 15:54:24 +0300

    Add project files.

---
+++ MagazaUygulamasi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace MagazaUygulamasi
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.ReadLine();
+
+        }
+    }
+}
